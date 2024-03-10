eccd71cfec4f6db67bb16b4299767db1bfa98d81
PrincessKuti <okaforprincessbc@gmail.com>
2024-03-10 13:43:22 +0000

    import lessons and checkout component into parent single file component

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,15 @@
     <h1 class="text-3xl font-bold underline">
       Hello world!
     </h1>
+
+    <Lessons />
+
+    <Checkout />
   </div>
 </template>
+
+<script setup>
+import Lessons from './components/Lessons.vue';
+import Checkout from './components/Checkout.vue';
+
+</script>
