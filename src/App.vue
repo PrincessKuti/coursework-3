ea2f7ac5dc6f6eb7b4c8fcfab6504dbbc9ec3e9f
PrincessKuti <okaforprincessbc@gmail.com>
2024-03-10 13:52:20 +0000

    add cart button in parent component to toggle cart display

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,43 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+  <main class="bg-gray-100">
+    <div class="px-6 mx-auto max-w-full lg:px-12 py-4">
 
-    <Lessons />
+      <div class="flex justify-end mb-8">
 
-    <Checkout />
-  </div>
+        <!-- "Shopping Cart" button -->
+        <button @click="toggleCart" :disabled="shoppingCart.length === 0"
+          class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 focus:outline-none focus:ring focus:border-blue-300 disabled:hover:bg-blue-500 disabled:opacity-50">
+          {{ showCart ? "Hide Cart" : "Show Cart" }}
+        </button>
+
+      </div>
+
+      <Lessons v-if="!showCart" />
+
+      <Checkout v-else class="mt-8" />
+    </div>
+  </main>
 </template>
 
-<script setup>
+<script>
 import Lessons from './components/Lessons.vue';
 import Checkout from './components/Checkout.vue';
+
+export default {
+  components: {
+    Lessons,
+    Checkout
+  },
+  data: () => ({
+    showCart: false,
+    shoppingCart: [],
+  }),
+  methods: {
+    toggleCart() {
+      // Toggle the visibility of the shopping cart
+      this.showCart = !this.showCart;
+    },
+  }
+}
 
 </script>
